74f41814e28971a9a6ba094ead5f3e362bcb1b7c
C659791 <vchannamsetty@bcbsm.com>
2025-09-03 14:16:24 -0500

    Sample for click not working on readonly input

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,48 +1,17 @@
 
-import { Component, inject } from '@angular/core';
-import {
-  RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent,
-  IonRefresher, IonRefresherContent, IonList, IonButton, LoadingController, IonSelect, IonSelectOption, IonItem
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonModal
 } from '@ionic/angular/standalone';
-import { MessageComponent } from '../message/message.component';
-
-import { DataService, Message } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, MessageComponent, IonButton, IonSelect,
-    IonSelectOption, IonItem, IonList],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonModal, IonInput]
 })
 export class HomePage {
-  private data = inject(DataService);
-  constructor(private loadingController: LoadingController) { }
 
-  refresh(ev: any) {
-    this.present();
-    setTimeout(async () => {
-      await this.dismiss();
-      setTimeout(() => {
-        (ev as RefresherCustomEvent).detail.complete();
-      }, 500);
-    }, 3000);
-  }
+  constructor() { }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
-
-  async present() {
-    const loadingElement = await this.loadingController.create({
-      cssClass: 'custom-loading',
-      spinner: 'crescent'
-    });
-    await loadingElement.present();
-  }
-
-  async dismiss() {
-    await this.loadingController.dismiss();
-
-  }
+ 
 }
